b8f98d39db0533b251a07d8e65a5cc34e8d7f9ff
HT-Jasper <jasper.huytran@gmail.com>
2026-02-15 21:26:59 -0800

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -17,3 +17,45 @@ skills.forEach((skill) => {
     skillItem.innerText = skill;
     skillsList.appendChild(skillItem);
 });
+
+const messageForm = document.querySelector('[name="leave_message"]');
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> <span>(${email})</span><p>${message}</p>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentElement;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    event.target.reset();
+});
+
+function updateMessage() {
+  const messagesSection = document.getElementById('messages');
+  const messageList = messagesSection.querySelector('ul');
+
+  if (messageList.children.length === 0) {
+    messagesSection.style.display = 'none';
+  } else {
+    messagesSection.style.display = 'block';
+  }
+}
+
+updateMessage();
